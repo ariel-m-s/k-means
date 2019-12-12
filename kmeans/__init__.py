f08db7b152b957ea1444b9936d1d7a104248b5fe
Ariel Martínez <amartinez5@uc.cl>
2019-12-12 18:32:06 -0300

    :recycle: refactor code

--- kmeans/__init__.py
+++ kmeans/__init__.py
@@ -7,9 +7,19 @@ class KMeansModel:
     def __init__(self):
         self.clusters = None
 
+    def update_clusters(self, source, n_clusters):
+        self.clusters = {i: [] for i in range(n_clusters)}
+
+        for vector in source:
+            centroid = get_closest_centroid(vector, self.centroids)
+            idx = self.centroids.index(centroid)
+
+            self.clusters[idx].append(vector)
+
     def update_centroids(self):
         if not self.clusters:
             raise AttributeError("cannot update centroids.")
+
         self.centroids = list(list(np.mean(self.clusters[cluster], axis=0))
                               for cluster in self.clusters)
 
@@ -18,13 +28,7 @@ class KMeansModel:
         self.centroids = get_centroids(n_clusters, source.shape[1])
 
         for _ in range(n_iter):
-            self.clusters = {i: [] for i in range(n_clusters)}
-
-            for vector in source:
-                centroid = get_closest_centroid(vector, self.centroids)
-                idx = self.centroids.index(centroid)
-                self.clusters[idx].append(vector)
-
+            self.update_clusters(source, n_clusters)
             self.update_centroids()
 
         return self.centroids, self.clusters
